d346cdb7c0b15266fc7185e38a28aaee7e4ab8e0
StevenG1997 <stivengarcia1855@gmail.com>
2023-09-22 09:34:56 -0500

    se cambio totalmente la forma de PAGO para que no se realice otra linea de la misma ID, sino que se coloque un coma 5

--- src/controllers/servicio.controller.js
+++ src/controllers/servicio.controller.js
@@ -691,7 +691,7 @@ exports.getByTerapeutaEncargadaFechaHoraInicioFechaHoraFin = (req, res) => {
   const sql = `	SELECT * FROM servicio WHERE terapeuta = ? AND encargada = ? 
     AND STR_TO_DATE(CONCAT(fecha,' ',horaStart),'%e-%m-%y %H:%i') >= ?
     AND STR_TO_DATE(CONCAT(fechaFin,' ',horaEnd),'%e-%m-%y %H:%i') <= ?
-    AND liquidadoTerapeuta = "0"`
+    AND liquidadoTerapeuta = "0" ORDER BY id desc`
 
   pool.query(
     sql, [terapeuta, encargada, `${fecha} ${horaStart}`, `${fechaFin} ${horaEnd}`],
@@ -711,7 +711,7 @@ exports.getByEncargadaFechaHoraInicioFechaHoraFin = (req, res) => {
   const sql = `	SELECT * FROM servicio WHERE encargada = ?
     AND STR_TO_DATE(CONCAT(fecha,' ',horaStart),'%e-%m-%y %H:%i') >= ?
     AND STR_TO_DATE(CONCAT(fechaFin,' ',horaEnd),'%e-%m-%y %H:%i') <= ?
-    AND liquidadoTerapeuta = "0"`
+    AND liquidadoTerapeuta = "0" ORDER BY id desc`
 
   pool.query(
     sql, [encargada, `${fecha} ${horaStart}`, `${fechaFin} ${horaEnd}`],
