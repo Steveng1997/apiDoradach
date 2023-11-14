2c906d79f79e6b8238f58d9dffd61d8f5c7303c9
StevenG1997 <stivengarcia1855@gmail.com>
2023-11-14 08:55:23 -0500

    adjust all 0.40

--- src/controllers/servicio.controller.js
+++ src/controllers/servicio.controller.js
@@ -722,9 +722,9 @@ exports.getFechaHoyAndManager = (req, res) => {
 exports.getFormaPago = (req, res) => {
   const { formaPago } = req.params;
 
-  const sql = `SELECT * FROM servicio WHERE formaPago LIKE '%${formaPago}%'`;
+  const sql = `SELECT * FROM servicio WHERE formaPago LIKE '%$?%'`;
 
-  pool.query(sql, [formaPago] , (err, result, fields) => {
+  pool.query(sql, [`${formaPago}`] , (err, result, fields) => {
     if (err) {
       throw err;
     }
