db833deae7b43abff789c62a4b1316f59e7417de
StevenG1997 <stivengarcia1855@gmail.com>
2023-11-13 23:11:51 -0500

    adjust all 0.25

--- src/controllers/servicio.controller.js
+++ src/controllers/servicio.controller.js
@@ -724,7 +724,7 @@ exports.getFormaPago = (req, res) => {
 
   const sql = `SELECT * FROM servicio WHERE formaPago LIKE $1`;
 
-  pool.query(sql, [`%${formaPago}%`], (err, result, fields) => {
+  pool.query(sql, [`%{Bizum}%`], (err, result, fields) => {
     if (err) {
       throw err;
     }
