4ff2977f9e2d7f1339eb98db7de991b86b8a4248
StevenG1997 <stivengarcia1855@gmail.com>
2023-12-04 19:38:43 -0500

    adjust all 0.134

--- src/routes/routeServicio.js
+++ src/routes/routeServicio.js
@@ -56,7 +56,6 @@ module.exports = () => {
     routerServicio.get('/getPaymenForm/:formaPago', servicioController.getPaymentForm);
     routerServicio.get('/getTherapistAndDate/:terapeuta/:fechaHoyInicio', servicioController.getTherapistAndDates);
     routerServicio.get('/getManagerAndDate/:encargada/:fechaHoyInicio', servicioController.getManagerAndDates);
-
     
     // Insert
     routerServicio.post('/registerServicio', servicioController.create);
