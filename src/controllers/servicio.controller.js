61f771869d3cda254b2747ce0576f3e085e00ed8
StevenG1997 <stivengarcia1855@gmail.com>
2023-12-20 18:22:31 -0500

    adjust all 0.182

--- src/controllers/servicio.controller.js
+++ src/controllers/servicio.controller.js
@@ -662,16 +662,17 @@ exports.getEncargadaFechaDescByCierreFalse = (req, res) => {
 };
 
 exports.getByTerapeutaEncargadaFechaHoraInicioFechaHoraFin = (req, res) => {
-  const { terapeuta, encargada, horaStart, horaEnd, fechaHoyInicio } = req.query;
+  const { terapeuta, encargada, horaStart, horaEnd, fecha, fechaFin } =
+    req.query;
 
   const sql = `	SELECT * FROM servicio WHERE terapeuta = ? AND encargada = ? 
-    AND STR_TO_DATE(CONCAT(fechaHoyInicio,' ',horaStart),'%e-%m-%y %H:%i') >= ?
-    AND STR_TO_DATE(CONCAT(fechaHoyInicio,' ',horaEnd),'%e-%m-%y %H:%i') <= ?
+    AND STR_TO_DATE(CONCAT(fecha,' ',horaStart),'%e-%m-%y %H:%i') >= ?
+    AND STR_TO_DATE(CONCAT(fechaFin,' ',horaEnd),'%e-%m-%y %H:%i') <= ?
     AND liquidadoTerapeuta = "0" ORDER BY id desc`;
 
   pool.query(
     sql,
-    [terapeuta, encargada, `${fechaHoyInicio} ${horaStart}`, `${fechaHoyInicio} ${horaEnd}`],
+    [terapeuta, encargada, `${fecha} ${horaStart}`, `${fechaFin} ${horaEnd}`],
     (err, result, fields) => {
       if (err) {
         throw err;
